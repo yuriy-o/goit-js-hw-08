498904ef3560dc68971d828fbaa2b207ec32a0b6
Yuriy <sharik.kiev@gmail.com>
2022-11-14 23:03:49 +0200

    add styles

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,9 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items';
+// Add imports above this line
+
 //! Change code below this line
-
-console.log(galleryItems);
-
 const imageGalleryEl = document.querySelector('.gallery');
 
 const galleryMarkup = onCreateImagesGallery(galleryItems);
