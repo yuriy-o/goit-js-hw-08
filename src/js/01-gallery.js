5a557013593374cf034727520fa7a71a56062362
Yuriy <sharik.kiev@gmail.com>
2022-11-14 21:31:22 +0200

    HW #01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
-import { galleryItems } from "./gallery-items";
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
 //! Change code below this line
 
 console.log(galleryItems);
+
+const imageGalleryEl = document.querySelector('.gallery');
+
+const galleryMarkup = onCreateImagesGallery(galleryItems);
+imageGalleryEl.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+function onCreateImagesGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+         <img loading="lazy"
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            width="354" height="240"
+            />
+</a>`;
+    })
+    .join('');
+}
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  overlayOpacity: 0.8,
+  closeText: '☣',
+  scrollZoom: false,
+});
