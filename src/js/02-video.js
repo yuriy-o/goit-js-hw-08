9b4bb01ac4f5412b5b03516f8bb38320abc89e2f
Yuriy <sharik.kiev@gmail.com>
2022-11-15 01:14:16 +0200

    HW #02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', function (e) {
+  const timePlay = e.seconds;
+  console.log(timePlay);
+
+  localStorage.setItem('videoplayer-current-time', timePlay);
+});
+
+const localTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(localTime); //задає плеєру час з локального сховища
