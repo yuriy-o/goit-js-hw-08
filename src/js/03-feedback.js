7653092ae6adfe17999c24ad0cf1823edf479ab5
Yuriy <sharik.kiev@gmail.com>
2022-11-15 21:50:25 +0200

    ex. 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,10 @@
+const formEl = document.querySelector('.feedback-form');
+console.log(formEl);
 
+formEl.addEventListener('input', func);
+
+function func(e) {
+  localStorage.setItem(e.target.name, e.target.value);
+
+  //   localStorage.setItem({ email: e.target.name, massege: e.target.value });
+}
