65c6811d7dcf5f0fd461652296d3dbde4ca5ab41
yuriy-o <82809592+yuriy-o@users.noreply.github.com>
2022-11-16 15:45:45 +0000

    Deploying to gh-pages from @ yuriy-o/goit-js-hw-08@fc8bcaf2a879092b025dd7b089d2c135043ebc1d 🚀

--- 03-feedback.3e4bd4d9.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form");console.log(e),e.addEventListener("input",(function(e){localStorage.setItem(e.target.name,e.target.value)}))}();
-//# sourceMappingURL=03-feedback.3e4bd4d9.js.map
--- 03-feedback.650b9e03.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form");console.log(e),e.addEventListener("input",(function(e){localStorage.setItem(e.target.name,e.target.value)}));
-//# sourceMappingURL=03-feedback.650b9e03.js.map
